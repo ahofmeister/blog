e9b756263ad41f2343c601a1a2c44ee8a1eef37a
Alexander Hofmeister <alexander.hofmeister@ebalance.ch>
2021-03-01 19:34:56 +0100

    Add copy to clipboard for code blocks

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    enableCopyCliboardForCodeBlocks();
+}
+
+
+function enableCopyCliboardForCodeBlocks() {
+    const code = document.querySelector('pre.highlight')
+    code.addEventListener('click', (ev) => {
+        setClipboard(ev.target.innerText)
+    })
+}
+
+function setClipboard(value) {
+    const tempInput = document.createElement("input");
+    tempInput.style = "position: absolute; left: -1000px; top: -1000px";
+    tempInput.value = value;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+}
